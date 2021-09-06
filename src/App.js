54e50ed482a78785b052b6358892047f0377902a
vilmosan <william.irons1992@gmail.com>
2021-09-06 20:28:48 +0200

    Fetching data, JSON-server, axios - Exercises

--- src/App.js
+++ src/App.js
@@ -6,19 +6,24 @@ import Person from './Person'
 import Phonebook from "./Phonebook";
 import Filter from "./Filter";
 import PersonForm from "./PersonForm";
+import axios from "axios";
 
 const App = () => {
-	const [ persons, setPersons ] = useState([
-		{ name: 'Arto Hellas', phone: '040-123456' },
-		{ name: 'Ada Lovelace', phone: '39-44-5323523' },
-		{ name: 'Dan Abramov', phone: '12-43-234345' },
-		{ name: 'Mary Poppendieck', phone: '39-23-6423122' }
-	])
+
+	const [ persons, setPersons ] = useState([])
 	const [ newName, setNewName ] = useState('')
 	const [ newPhone, setNewPhone ] = useState('')
 	const [ searchTerm, setSearchTerm ] = useState('')
 	const [ filteredPersonList, setFilteredPersonList ] = useState([])
 	const [showAll, setShowAll] = useState(true)
+
+	const initPersons = () => {
+		axios.get('http://localhost:3001/persons').then(response =>{
+			setPersons(response.data);
+		});
+	}
+
+	useEffect(initPersons,[]);
 
 	const addContact = (event) => {
 		event.preventDefault()
